b74aedcbc0ca8e63a8e0aa2b1c31d1a5595b33f7
Domingo Gallardo <domingo.gallardo@ua.es>
2020-10-27 14:06:28 +0100

    Extract method

--- src/Customer.java
+++ src/Customer.java
@@ -25,12 +25,8 @@ class Customer {
         while (allRentals.hasMoreElements()) {
             double thisAmount = 0;
             Rental each = (Rental) allRentals.nextElement();
+            frequentRenterPoints += each.getFrequentRenterPoints();
 
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
