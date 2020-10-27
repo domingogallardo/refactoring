84a47564cadbd36baaeefb4135f5ab209c4785cc
Domingo Gallardo <domingo.gallardo@ua.es>
2020-10-27 13:29:43 +0100

    Primer commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Movie tenet = new Movie("Tenet", Movie.NEW_RELEASE);
+        Movie busan = new Movie("Train to Busan", Movie.REGULAR);
+        Movie padre = new Movie("Padre no hay más que uno", Movie.CHILDRENS);
+
+        Rental rental1 = new Rental(tenet, 2);
+        Rental rental2 = new Rental(busan, 2);
+        Rental rental3 = new Rental(padre, 1);
+
+        Customer customer = new Customer("domingogallardo");
+
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        customer.addRental(rental3);
+
+        System.out.println(customer.statement());
+    }
+}
