a897c9cd251f75f792b54a6314bced2b7c2d3712
Domingo Gallardo <domingo.gallardo@ua.es>
2020-11-12 11:36:30 +0100

    Replace Type Code with State

---
+++ src/Price.java
@@ -0,0 +1,21 @@
+abstract public class Price {
+    abstract int getPriceCode();
+}
+
+class ChildrensPrice extends Price {
+    int getPriceCode() {
+        return Movie.CHILDRENS;
+    }
+}
+
+class NewReleasePrice extends Price {
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+}
+
+class RegularPrice extends Price {
+    int getPriceCode() {
+        return Movie.REGULAR;
+    }
+}
