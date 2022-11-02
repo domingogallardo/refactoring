0a5c85ba12f0e94bb8acce570bd06144d5378742
Domingo Gallardo <domingo.gallardo@ua.es>
2022-11-02 19:30:42 +0100

    Paso 5 - Replace Temp with Query

--- src/main/java/org/example/Customer.java
+++ src/main/java/org/example/Customer.java
@@ -28,8 +28,6 @@ class Customer {
             double thisAmount = 0;
             Rental each = (Rental) allRentals.nextElement();
 
-            thisAmount = each.getCharge();
-
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
@@ -37,8 +35,8 @@ class Customer {
                     each.getDaysRented() > 1) frequentRenterPoints++;
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
+            totalAmount += each.getCharge();
 
         }
         //add footer lines
